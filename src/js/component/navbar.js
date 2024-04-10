f827ddf51574c6888d5839155c690ddae8d65479
Anibal-JPG <109487916+Anibal-JPG@users.noreply.github.com>
2024-04-10 04:39:42 +0000

    1 parte solo bootstrap de momento

--- src/js/component/navbar.js
+++ src/js/component/navbar.js
@@ -2,16 +2,21 @@ import React from "react";
 import { Link } from "react-router-dom";
 
 export const Navbar = () => {
-	return (
-		<nav className="navbar navbar-light bg-light mb-3">
-			<Link to="/">
-				<span className="navbar-brand mb-0 h1">React Boilerplate</span>
-			</Link>
-			<div className="ml-auto">
-				<Link to="/demo">
-					<button className="btn btn-primary">Check the Context in action</button>
-				</Link>
+	return <nav className="navbar navbar-expand-lg navbar-light bg-light">
+		<div className="container-fluid">
+			<a className="navbar-brand" href="#">star wars</a>
+			<div className="collapse navbar-collapse justify-content-end" id="navbarNavAltMarkup">
+				<div className="navbar-nav">
+					<div className="dropdown">
+						<button className="btn btn-secondary dropdown-toggle" type="button" id="dropdownMenuButton1" data-bs-toggle="dropdown" aria-expanded="false">
+							Favorites
+						</button>
+						<ul className="dropdown-menu dropdown-menu-end" aria-labelledby="dropdownMenuButton1">
+							{/* aqui va lo de los favoritos */}
+						</ul>
+					</div>
+				</div>
 			</div>
-		</nav>
-	);
+		</div>
+	</nav>
 };
